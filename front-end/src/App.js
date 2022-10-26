5f1f04cb4d637d27ec9923ed308fcd114a307d11
Abdul Samad Gomda <ag7766@nyu.edu>
2022-10-26 16:32:08 -0400

    Moved explore page from App.js to explore.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavbarComponent from "./components/navbar";
-import Footer from "./components/footer";
-import Card from "./components/card";
 import React from "react";
-import axios from "axios";
-import SearchBarComponent from "./components/searchbar";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //Imported for routes
 import ProfilePage from "./components/ProfilePage";
@@ -14,141 +10,15 @@ import EditImage from "./components/EditImage";
 import Group from "./components/group";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Explore from "./explore";
 
-// mock data
-const rand = function () {
-	return Math.floor(Math.random() * 2);
-};
-
-// Mock data to test in development
-// const data = [
-// 	{
-// 		date: "07/17/2022",
-// 		restaurant: "Runte-Rosenbaum",
-// 		location: "Lishu",
-// 		remaining: 8,
-// 		capacity: 4,
-// 		cuisine: "China",
-// 		dress_code: "Formal-Casual",
-// 		budget: 2,
-// 		name: "Kenneth Pauletto",
-// 	},
-// 	{
-// 		date: "09/13/2022",
-// 		restaurant: "Bernier and Sons",
-// 		location: "Majunying",
-// 		remaining: 2,
-// 		capacity: 4,
-// 		cuisine: "China",
-// 		dress_code: "Casual",
-// 		budget: 3,
-// 		name: "Arel Josey",
-// 	},
-// 	{
-// 		date: "12/05/2021",
-// 		restaurant: "Rodriguez, Denesik and White",
-// 		location: "Longlisuo",
-// 		remaining: 1,
-// 		capacity: 38,
-// 		cuisine: "China",
-// 		dress_code: "Casual",
-// 		budget: 5,
-// 		name: "Alena Chretien",
-// 	},
-// 	{
-// 		date: "10/08/2022",
-// 		restaurant: "Harris, Goyette and Denesik",
-// 		location: "Roshal’",
-// 		remaining: 19,
-// 		capacity: 27,
-// 		cuisine: "Russia",
-// 		dress_code: "Casual",
-// 		budget: 1,
-// 		name: "Alvira Carlisi",
-// 	},
-// 	{
-// 		date: "02/12/2022",
-// 		restaurant: "Lang LLC",
-// 		location: "Akune",
-// 		remaining: 14,
-// 		capacity: 3,
-// 		cuisine: "Japan",
-// 		dress_code: "Casual",
-// 		budget: 5,
-// 		name: "Drew Josipovitz",
-// 	},
-// 	{
-// 		date: "10/20/2021",
-// 		restaurant: "Spinka, Harber and Carter",
-// 		location: "Sagua la Grande",
-// 		remaining: 11,
-// 		capacity: 32,
-// 		cuisine: "Cuba",
-// 		dress_code: "Casual",
-// 		budget: 4,
-// 		name: "Frederich Mizen",
-// 	},
-// 	{
-// 		date: "09/30/2022",
-// 		restaurant: "Sauer, Hane and Lindgren",
-// 		location: "Ajuda",
-// 		remaining: 8,
-// 		capacity: 42,
-// 		cuisine: "Portugal",
-// 		dress_code: "Casual",
-// 		budget: 5,
-// 		name: "Marlyn Giovanazzi",
-// 	},
-// 	{
-// 		date: "04/01/2022",
-// 		restaurant: "Grimes-Berge",
-// 		location: "Voskresenskoye",
-// 		remaining: 12,
-// 		capacity: 35,
-// 		cuisine: "Russia",
-// 		dress_code: "Formal-Casual",
-// 		budget: 3,
-// 		name: "Bianka Scarth",
-// 	},
-// 	{
-// 		date: "04/04/2022",
-// 		restaurant: "Romaguera, Bartoletti and Lind",
-// 		location: "San Cristóbal",
-// 		remaining: 19,
-// 		capacity: 25,
-// 		cuisine: "Dominican Republic",
-// 		dress_code: "Casual",
-// 		budget: 3,
-// 		name: "Wright Rissom",
-// 	},
-// 	{
-// 		date: "11/21/2021",
-// 		restaurant: "Prohaska-Lockman",
-// 		location: "Huadian",
-// 		remaining: 4,
-// 		capacity: 34,
-// 		cuisine: "China",
-// 		dress_code: "Formal-Casual",
-// 		budget: 2,
-// 		name: "Niel McGrudder",
-// 	},
-// ];
 function App() {
-	const [cardData, setCardData] = React.useState(null);
-
-	React.useEffect(() => {
-		axios
-			.get(`https://my.api.mockaroo.com/mealhub.json?key=2f898fd0`)
-			.then((response) => {
-				setCardData(response.data);
-			});
-	}, []);
-
 	return (
 		<div>
 			{/*List of Routes*/}
 			<Router>
 				<Routes>
+					<Route path="/" element={<Explore />} />
 					<Route path="/profilePage" element={<ProfilePage />} />
 					<Route path="/editProfilePage" element={<EditProfilePage />} />
 					<Route path="/addImage" element={<EditImage />} />
@@ -157,23 +27,6 @@ function App() {
 					<Route path="/Register" element={<Register />} />
 				</Routes>
 			</Router>
-
-			<NavbarComponent />
-			<SearchBarComponent />
-			<h1 className="main-heading pt-5">Suggestions</h1>
-			<div>
-				{cardData &&
-					cardData.map((e) => {
-						if (rand()) {
-							e["image"] = "https://picsum.photos/200";
-						} else {
-							e["image"] = "https://random.imagecdn.app/200/200";
-						}
-
-						return <Card data={e} />;
-					})}
-			</div>
-			<Footer />
 		</div>
 	);
 }
--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,28 +1,40 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import Logo from '../assets/logo.png'
-import './navbar.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import Logo from "../assets/logo.png";
+import "./navbar.css";
 
 function NavbarComponent() {
-    return (
-        <Navbar collapseOnSelect expand="lg" sticky="top" style={{borderBottom: '1px solid #f0f0f0', backgroundColor: '#fff'}}>
-            <Container>
-                <Navbar.Brand href="#home"><img src={Logo} height="35" alt="MealHub"/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="./createGroup" className='custom-start ms-2 me-2'><b>Start a new group</b></Nav.Link>
-                        <Nav.Link href="./Register" className='custom-signup ms-2 me-2'>Sign up</Nav.Link>
-                        <Button href="./Login" className='custom-btn ms-2 me-2'>Sign in</Button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+	return (
+		<Navbar
+			collapseOnSelect
+			expand="lg"
+			sticky="top"
+			style={{ borderBottom: "1px solid #f0f0f0", backgroundColor: "#fff" }}
+		>
+			<Container>
+				<Navbar.Brand href="/">
+					<img src={Logo} height="35" alt="MealHub" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto"></Nav>
+					<Nav>
+						<Nav.Link href="./createGroup" className="custom-start ms-2 me-2">
+							<b>Start a new group</b>
+						</Nav.Link>
+						<Nav.Link href="./Register" className="custom-signup ms-2 me-2">
+							Sign up
+						</Nav.Link>
+						<Button href="./Login" className="custom-btn ms-2 me-2">
+							Sign in
+						</Button>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default NavbarComponent;
