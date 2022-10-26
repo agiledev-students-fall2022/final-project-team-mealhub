e8e9311c23b70d3bf5fe1d9197b069bea64d47a2
Abdul Samad Gomda <ag7766@nyu.edu>
2022-10-26 16:16:57 -0400

    Added pop-up functionality to the cards, fixed join button on mobile screen

--- front-end/src/components/card.js
+++ front-end/src/components/card.js
@@ -2,29 +2,53 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import { Button, Item } from "semantic-ui-react";
 import "./card.css";
+import Cardmodal from "./cardmodal";
 
-const Card = ({ data }) => (
-	<div className="card-component">
-		<Item.Group>
-			<Item>
-				<Item.Image size="small" src={data.image} />
+function Card({ data, key }) {
+	// this.state = {
+	// 	modalOpen: false,
+	// };
+	const [open, setOpen] = React.useState(false);
+	return (
+		<div className="card-component">
+			<Item.Group>
+				<Item>
+					<Item.Image size="small" src={"https://picsum.photos/200"} />
 
-				<Item.Content verticalAlign="middle">
-					<Item.Meta>
-						<span>{data.date}</span>
-					</Item.Meta>
-					<Item.Header as="a">{data.restaurant}</Item.Header>
-					<Item.Description>{data.location}</Item.Description>
-					<Item.Meta>
-						<span>{data.capacity + " spots left"}</span>
-					</Item.Meta>
-					<Item.Extra>
-						<Button floated="right">Join</Button>
-					</Item.Extra>
-				</Item.Content>
-			</Item>
-		</Item.Group>
-	</div>
-);
+					<Item.Content>
+						<Item.Meta>
+							<span>{data.date}</span>
+						</Item.Meta>
+						<Item.Header as="a">{data.restaurant}</Item.Header>
+						<Item.Description>{data.location}</Item.Description>
+						<Item.Meta>
+							<span>{data.remaining + " spots left"}</span>
+						</Item.Meta>
+						<Item.Extra>
+							<Button
+								floated="right"
+								onClick={() => {
+									setOpen(true);
+									console.log("clicked");
+								}}
+							>
+								Join
+							</Button>
+						</Item.Extra>
+					</Item.Content>
+				</Item>
+			</Item.Group>
+
+			<Cardmodal // The invisible modal itself
+				key={key}
+				modalOpen={open}
+				handleClose={() => {
+					setOpen(false);
+				}}
+				data={data}
+			/>
+		</div>
+	);
+}
 
 export default Card;
