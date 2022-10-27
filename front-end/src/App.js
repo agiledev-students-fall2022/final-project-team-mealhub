2aa5c430e763763a80b350271c3f29392dcd2cf1
mishaseo <ms11568@nyu.edu>
2022-10-26 20:19:02 -0400

    Moved pages to the pages folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,9 +4,9 @@ import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //Imported for routes
-import ProfilePage from "./components/ProfilePage";
-import EditProfilePage from "./components/EditProfilePage";
-import EditImage from "./components/EditImage";
+import ProfilePage from "./pages/ProfilePage";
+import EditProfilePage from "./pages/EditProfilePage";
+import EditImage from "./pages/EditImage";
 import Group from "./components/group";
 import Login from "./components/Login";
 import Register from "./components/Register";
---
+++ front-end/src/pages/EditProfilePage.js
@@ -0,0 +1,133 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./ProfilePage.css";
+import Image from "react-bootstrap/Image";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ProfileNavbarComponent from "../components/ProfileNavbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Footer from "../components/footer";
+
+function EditProfilePage() {
+  const [data, setData] = useState([]);
+
+  //render profile data onto the page
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        "https://my.api.mockaroo.com/user_profiles.json?key=18bea250"
+      );
+      // set the data assuming the first person is the user
+      setData(result.data[0]);
+    }
+    fetchData();
+  }, []);
+  //---------------Form validation--------------------------------
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  //---------------------------returning------------------------------
+  return (
+    <div>
+      <ProfileNavbarComponent />
+      <div id="title">
+        <br></br>
+        <p>Edit Profile</p>
+      </div>
+
+      <div id="pfImg">
+        <Image src="https://picsum.photos/100/100" roundedCircle height="140" width="140" />
+      </div>
+
+      <div id="changeImageText">
+        <Link to={"/addImage"}>Change Profile Image</Link>
+      </div>
+
+      {/*------------------------------Form section------------------------*/}
+      <div id="infoEdit">
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Name"
+              defaultValue={data.name}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide your name.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Email"
+              defaultValue={data.email}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide your email address.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Age</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Age"
+              defaultValue={data.age}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide your age.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Location</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Location"
+              defaultValue={data.location}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide your current location.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Cuisine Preferences</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Cuisine preferences (Ex: Korean)"
+              defaultValue={data.cuisinePreferences}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide at least one cuisine preference.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <br></br>
+          <Button type="submit" variant="primary">
+            Save
+          </Button>{" "}
+          <Button href={"/profilePage"} variant="secondary">
+            Done
+          </Button>{" "}
+        </Form>
+      </div>
+      < Footer/>
+    </div>
+  );
+}
+
+export default EditProfilePage;
