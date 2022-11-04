970aced8ee0bde0fd3acaa521746b8ba4a84d928
Abdul Samad Gomda <ag7766@nyu.edu>
2022-11-04 17:54:14 -0400

    added dotenv file for secrets

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,16 @@
-const server = require("./app") // load up the web server
-const port = 3000 // the port to listen to for incoming requests
+const server = require("./app"); // load up the web server
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
 // call express's listen function to start listening to the port
-const listener = server.listen(port, function () {
-  console.log(`Server running on port: ${port}`)
-})
+const listener = server.listen(PORT, function () {
+	console.log(`Server running on port: ${port}`);
+});
 // Stop listening to the port
 const close = () => {
-  listener.close()
-}
+	listener.close();
+};
 module.exports = {
-  close: close,
-}
+	close: close,
+};
