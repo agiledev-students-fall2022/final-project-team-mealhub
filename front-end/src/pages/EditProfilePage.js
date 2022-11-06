ffc395c91360d91637941d20ec04b7c75fb7f139
mishaseo <ms11568@nyu.edu>
2022-11-05 23:54:23 -0400

    Connected front-end page to server

--- front-end/src/pages/EditProfilePage.js
+++ front-end/src/pages/EditProfilePage.js
@@ -8,126 +8,157 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import Footer from "../components/footer";
+import { useNavigate } from "react-router-dom";
 
 function EditProfilePage() {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [age, setAge] = useState("");
+    const [location, setLocation] = useState("");
 
-  //render profile data onto the page
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios(
-        "https://my.api.mockaroo.com/user_profiles.json?key=18bea250"
-      );
-      // set the data assuming the first person is the user
-      setData(result.data[0]);
-    }
-    fetchData();
-  }, []);
-  //---------------Form validation--------------------------------
-  const [validated, setValidated] = useState(false);
+    //WILL BE USED WHEN DATABASE IS INTEGRATED
+    //const [cuisinePreferences, setCusinePreferences] = useState([]);
+    const [cuisinePreferences, setCusinePreferences] = useState("");
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    //-------------------render profile data onto the page-------------------------
+    const fetchProfileData = async () => {
+        try {
+            //get data for id=2 (placeholder)
+            const response = await axios.get(
+                "http://localhost:8080/profilePage/2"
+            );
+            setData(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-    setValidated(true);
-  };
+    useEffect(() => {
+        fetchProfileData();
+    }, []);
+    //---------------Form validation--------------------------------
 
-  //---------------------------returning------------------------------
-  return (
-    <div>
-      <ProfileNavbarComponent />
-      <div id="title">
-        <br></br>
-        <p>Edit Profile</p>
-      </div>
+    const navigate = useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let newData = {
+            id: 2,
+            name: name,
+            email: email,
+            age: age,
+            location: location,
+            cuisinePreferences: cuisinePreferences,
+        };
+        if (newData.name == "") newData.name = data.name;
+        if (newData.email == "") newData.email = data.email;
+        if (newData.age == "") newData.age = data.age;
+        if (newData.location == "") newData.location = data.location;
+        if (newData.cuisinePreferences == "")
+            newData.cuisinePreferences = data.cuisinePreferences;
 
-      <div id="pfImg">
-        <Image src="https://picsum.photos/100/100" roundedCircle height="140" width="140" />
-      </div>
+        axios
+            // post new message to server
+            .post("http://localhost:8080/editInfo", newData)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
 
-      <div id="changeImageText">
-        <Link to={"/addImage"}>Change Profile Image</Link>
-      </div>
+        navigate("/profilePage");
+    };
 
-      {/*------------------------------Form section------------------------*/}
-      <div id="infoEdit">
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Form.Group>
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Name"
-              defaultValue={data.name}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide your name.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Email"
-              defaultValue={data.email}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide your email address.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Age</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Age"
-              defaultValue={data.age}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide your age.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Location</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Location"
-              defaultValue={data.location}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide your current location.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Cuisine Preferences</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Cuisine preferences (Ex: Korean)"
-              defaultValue={data.cuisinePreferences}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide at least one cuisine preference.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <br></br>
-          <Button type="submit" variant="primary">
-            Save
-          </Button>{" "}
-          <Button href={"/profilePage"} variant="secondary">
-            Done
-          </Button>{" "}
-        </Form>
-      </div>
-      < Footer/>
-    </div>
-  );
+    //---------------------------returning------------------------------
+    return (
+        <div>
+            <ProfileNavbarComponent />
+            <div id="title">
+                <br></br>
+                <p>Edit Profile</p>
+            </div>
+
+            <div id="pfImg">
+                <Image
+                    src="https://picsum.photos/100/100"
+                    roundedCircle
+                    height="140"
+                    width="140"
+                />
+            </div>
+
+            <div id="changeImageText">
+                <Link to={"/addImage"}>Change Profile Image</Link>
+            </div>
+
+            {/*------------------------------Form section------------------------*/}
+            <div id="infoEdit">
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Name"
+                            defaultValue={data.name}
+                            required
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Email"
+                            defaultValue={data.email}
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Age</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Age"
+                            defaultValue={data.age}
+                            required
+                            onChange={(e) => setAge(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Location"
+                            defaultValue={data.location}
+                            required
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Cuisine Preferences</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Cuisine preferences (Ex: Korean)"
+                            defaultValue={data.cuisinePreferences}
+                            required
+                            onChange={(e) =>
+                                setCusinePreferences(e.target.value)
+                            }
+                        />
+                    </Form.Group>
+                    <br></br>
+                    <Button type="submit" variant="primary">
+                        Save
+                    </Button>{" "}
+                    <Button href={"/profilePage"} variant="secondary">
+                        Done
+                    </Button>{" "}
+                </Form>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default EditProfilePage;
--- front-end/src/pages/ProfilePage.js
+++ front-end/src/pages/ProfilePage.js
@@ -10,62 +10,78 @@ import Container from "react-bootstrap/esm/Container";
 import Button from "react-bootstrap/Button";
 
 function ProfilePage() {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  //render profile data onto the page
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios(
-        "https://my.api.mockaroo.com/user_profiles.json?key=18bea250"
-      );
-      // set the data assuming the first person is the user
-      setData(result.data[0]);
-    }
-    fetchData();
-  }, []);
+    //render profile data onto the page
+    const fetchProfileData = async () => {
+        try {
+            //get data for id=2 (placeholder)
+            const response = await axios.get(
+                "http://localhost:8080/profilePage/2"
+            );
+            setData(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-  return (
-    <Container>
-      <ProfileNavbarComponent />
-      <div id="title">
-        <br></br>
-        <p>My Profile</p>
-      </div>
-      <div id="pfImg">
-        <Image src="https://picsum.photos/100/100" roundedCircle height="140" width="140" />
-      </div>
-      <div id="info">
-        <Form.Group>
-          <Form.Label>Name</Form.Label>
-          <Form.Control placeholder={data.name} disabled />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Email</Form.Label>
-          <Form.Control placeholder={data.email} disabled />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Age</Form.Label>
-          <Form.Control placeholder={data.age} disabled />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Location</Form.Label>
-          <Form.Control placeholder={data.location} disabled />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Cuisine Preferences</Form.Label>
-          <Form.Control placeholder={data.cuisinePreferences} disabled />
-        </Form.Group>
-        <br></br>
-      </div>
+    useEffect(() => {
+        fetchProfileData();
+    }, []);
 
-      <div id="button">
-        <Button href={"/editProfilePage"} className="custom-btn ms-2 me-2">
-          Edit profile
-        </Button>
-      </div>
-      < Footer/>
-    </Container>
-  );
+    return (
+        <Container>
+            <ProfileNavbarComponent />
+            <div id="title">
+                <br></br>
+                <p>My Profile</p>
+            </div>
+            <div id="pfImg">
+                <Image
+                    src="https://picsum.photos/100/100"
+                    roundedCircle
+                    height="140"
+                    width="140"
+                />
+            </div>
+            <div id="info">
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control placeholder={data.name} disabled />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control placeholder={data.email} disabled />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Age</Form.Label>
+                    <Form.Control placeholder={data.age} disabled />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Location</Form.Label>
+                    <Form.Control placeholder={data.location} disabled />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Cuisine Preferences</Form.Label>
+                    <Form.Control
+                        placeholder={data.cuisinePreferences}
+                        disabled
+                    />
+                </Form.Group>
+                <br></br>
+            </div>
+
+            <div id="button">
+                <Button
+                    href={"/editProfilePage"}
+                    className="custom-btn ms-2 me-2"
+                >
+                    Edit profile
+                </Button>
+            </div>
+            <Footer />
+        </Container>
+    );
 }
 
 export default ProfilePage;
