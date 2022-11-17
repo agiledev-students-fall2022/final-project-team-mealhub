5a3eef972840600a7f7e9f311b544c993eae4724
Dev Kalavadiya <67102702+Dev-Kalavadia@users.noreply.github.com>
2022-11-16 20:34:13 -0500

    Set Data to Database
    Tested the group data saved in the mongoDB

--- back-end/routes/createGroup.js
+++ back-end/routes/createGroup.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const axios = require("axios");
 const { check, validationResult } = require("express-validator");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const Group = require("../models/Group");
+
+mongoose.connect('mongodb+srv://mealhub12345:mealhub12345@cluster0.rx68d3c.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 // router.use(cors());
 router.use(cors());
@@ -19,15 +28,15 @@ router.use((req, res, next) => {
   next();
 });
 
-
+const groupData = [];
 
 router.use(express.json());
 router.post("/", (req, res) => {
-  const groupData = [];
+  
   groupData.push(req.body);
-  res.send({groupData});
+  res.send({ groupData });
 
-  let title = req.body.restaurant
+  let title = req.body.restaurant;
   let description = req.body.description;
   let capacity = req.body.attendees;
   let budget = req.body.budgetDollar;
@@ -36,66 +45,71 @@ router.post("/", (req, res) => {
   let time = req.body.time;
   let cuisine = req.body.cuisine.toLowerCase();
   // let user = req.body.user;
-  let location = req.body.location
+  let location = req.body.location;
 
-  // console.log(req.body)
+  const group = new Group({
+    title,
+    description,
+    capacity,
+    budget,
+    dress_code,
+    date,
+    time,
+    cuisine,
+    // user,
+    location,
 
-  // let members = req.body.members;
+  }
+  );
+  group.save()
+    .then(result => {
+      console.log(result);
+    }
+    )
+    .catch(err => console.log(err));
 
 
-  //destructure groupData into individual vairables
-  // const { name, description, location, category, image } = groupData;
-  // console.log(name, description, location, category, image);
-  // console.log(name, description)
 
 });
 
+// router.post("/", (req, res) => {
+//   const groupData = [];
+//   groupData.push(req.body);
+//   res.send({groupData});
 
+//   let title = req.body.restaurant
+//   let description = req.body.description;
+//   let capacity = req.body.attendees;
+//   let budget = req.body.budgetDollar;
+//   let dress_code = req.body.dress;
+//   let date = req.body.date;
 
+//   let time = req.body.time;
+//   let cuisine = req.body.cuisine.toLowerCase();
+//   // let user = req.body.user;
+//   let location = req.body.location
 
-
-
-
-
-
-// router.post("/group", [
-//     check("groupName", "Group name is required").not().isEmpty(),
-//     check("groupDescription", "Group description is required").not().isEmpty(),
-//     check("groupCategory", "Group category is required").not().isEmpty(),
-//     check("groupLocation", "Group location is required").not().isEmpty(),
-//     check("groupSize", "Group size is required").not().isEmpty(),
-//     check("groupDate", "Group date is required").not().isEmpty(),
-//     check("groupTime", "Group time is required").not().isEmpty(),
-//     check("groupDuration", "Group duration is required").not().isEmpty(),
-//     check("groupPrice", "Group price is required").not().isEmpty(),
-
-// ], (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         return res.status(400).json({ errors: errors.array() });
+//   // Send group Data to MongoDB URL collection
+//   axios
+//     .post(MONGO_URI, {
+//       title,
+//       description,
+//       capacity,
+//       budget,
+//       dress_code,
+//       date,
+//       time,
+//       cuisine,
+//       location
+//     })
+//     .then((response) => {
+//       console.log(response);
 //     }
-//     const { groupName, groupDescription, groupCategory, groupLocation, groupSize, groupDate, groupTime, groupDuration, groupPrice } = req.body;
-//     const group = {
-//         groupName,
-//         groupDescription,
-//         groupCategory,
-//         groupLocation,
-//         groupSize,
-//         groupDate,
-//         groupTime,
-//         groupDuration,
-//         groupPrice
+//     )
+//     .catch((error) => {
+//       console.log(error);
 //     }
-//     // axios.post("---------MongoDB Database here------", group)
-//     console.log(group)
-
-//         .then(function (response) {
-//             console.log(response);
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-//     res.redirect("/createGroup");
+//     );
 // });
 
 module.exports = router;
