20833b70bf3e872f3ea2b7d97bb2c740b2e912fa
mishaseo <ms11568@nyu.edu>
2022-11-21 15:46:34 -0500

    Removed unused variables

--- back-end/routes/profilePage.js
+++ back-end/routes/profilePage.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const axios = require("axios");
-
 const User = require("../models/User");
 
 const passport = require("passport");
