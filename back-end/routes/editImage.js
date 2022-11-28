c1384a28e712217b55b05969f51bed1af7c62664
mishaseo <ms11568@nyu.edu>
2022-11-28 16:10:38 -0500

    Added default functionality when no image added

--- back-end/routes/editImage.js
+++ back-end/routes/editImage.js
@@ -4,17 +4,12 @@ const multer = require("multer");
 
 const User = require("../models/User");
 
-const passport = require("passport");
-
-router.use(passport.initialize());
-
-//const { jwtOptions, jwtStrategy } = require("../jwt-config.js"); // import setup options for using JWT in passport
-//passport.use(jwtStrategy);
+//const { checkUser } = require("../middleware_auth/jwt_auth");
 
 const upload = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, "public/uploads");
+            cb(null, "../front-end/public/uploads");
         },
         filename: function (req, file, cb) {
             cb(null, `${file.fieldname}-${Date.now()}_${file.originalname}`);
@@ -33,10 +28,12 @@ const upload = multer({
     },
 });
 
+//CODE FOR WHEN AUTHENTICATION IS SET
+/*
 // route for HTTP POST requests for /upload-example
 router.post(
     "/uploadImage",
-    passport.authenticate("jwt", { session: false }),
+    checkUser,
     upload.single("image"),
     async (req, res, next) => {
         // check whether anything was uploaded
@@ -49,17 +46,62 @@ router.post(
             };
             // requesting and updating resource from DB
             try {
-                const userInfo = await User.findByIdAndUpdate(req.user.id, {
-                    image: req.file.path,
+                await User.findByIdAndUpdate(req.user.id, {
+                    image: "/uploads/" + req.file.filename,
                 });
             } catch (err) {
-                console.log("edit profile error ");
                 res.json(err);
             }
 
             res.json(data); // send response
+        } else {
+            try {
+                await User.findByIdAndUpdate(req.user.id, {
+                    image: "/uploads/defaultProfilePic.png",
+                });
+            } catch (err) {
+                res.json(err);
+            }
         }
     }
 );
+
+*/
+
+router.post("/uploadImage", upload.single("image"), async (req, res, next) => {
+    // check whether anything was uploaded
+    if (req.file) {
+        // success! send data back to the client, e.g. some JSON data
+        const data = {
+            status: "success",
+            message: "The files were uploaded!!!",
+            files: req.file,
+        };
+        // requesting and updating resource from DB
+        try {
+            await User.findOneAndUpdate(
+                { firstName: "Hello" },
+                {
+                    image: "/uploads/" + req.file.filename,
+                }
+            );
+        } catch (err) {
+            res.json(err);
+        }
+
+        res.json(data); // send response
+    } else {
+        try {
+            await User.findOneAndUpdate(
+                { firstName: "Hello" },
+                {
+                    image: "/uploads/defaultProfilePic.png",
+                }
+            );
+        } catch (err) {
+            res.json(err);
+        }
+    }
+});
 
 module.exports = router;
