1dd86f21726444bdfebf488b68310328489d6184
mishaseo <ms11568@nyu.edu>
2022-11-28 16:11:50 -0500

    Added a mock user to fetch info from databse

--- back-end/routes/editProfilePage.js
+++ back-end/routes/editProfilePage.js
@@ -4,57 +4,71 @@ const router = express.Router();
 
 const User = require("../models/User");
 
-const passport = require("passport");
-
-router.use(passport.initialize());
-
-//const { jwtOptions, jwtStrategy } = require("../jwt-config.js"); // import setup options for using JWT in passport
-//passport.use(jwtStrategy);
+//const { checkUser } = require("../middleware_auth/jwt_auth");
 
 const { body, validationResult } = require("express-validator");
+
+//CODE FOR WHEN AUTHENTICATION IS SET
+/*router.post(
+    "/editInfo",
+    checkUser,
+    body("displayName")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("Must be at least 1 character long"),
+
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            } else {
+                // requesting resource from DB
+                const dataRecieved = {
+                    status: "success!",
+                    your_data: {
+                        displayName: req.body.displayName.trim(),
+                    },
+                };
+                await User.findByIdAndUpdate(req.user.id, {
+                    displayName: req.body.displayName.trim(),
+                });
+                res.json(dataRecieved);
+            }
+        } catch (err) {
+            console.log(err);
+            res.json({
+                success: false,
+            });
+        }
+    }
+);*/
 
 router.post(
     "/editInfo",
     body("displayName")
         .trim()
         .isLength({ min: 1 })
-        .withMessage("Must be at least 1 character long")
-        .isAlpha()
-        .withMessage("Must be alphabetic"),
-    body("firstName")
-        .trim()
-        .isLength({ min: 1 })
-        .withMessage("Must be at least 1 character long")
-        .isAlpha()
-        .withMessage("Must be alphabetic"),
-    body("lastName")
-        .trim()
-        .isLength({ min: 1 })
-        .withMessage("Must be at least 1 character long")
-        .isAlpha()
-        .withMessage("Must be alphabetic"),
-    passport.authenticate("jwt", { session: false }),
+        .withMessage("Must be at least 1 character long"),
     async (req, res) => {
         try {
-            // requesting resource from DB
-            const userInfo = await User.findOne(req.user.id);
-            const dataRecieved = {
-                status: "success!",
-                your_data: {
-                    displayName: req.body.displayName.trim(),
-                    firstName: req.body.firstName.trim(),
-                    lastName: req.body.lastName.trim(),
-                },
-            };
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return res.status(400).json({ errors: errors.array() });
             } else {
-                const userInfo = await User.findByIdAndUpdate(req.user.id, {
-                    displayName: req.body.displayName.trim(),
-                    firstName: req.body.firstName.trim(),
-                    lastName: req.body.lastName.trim(),
-                });
+                // requesting resource from DB
+                const dataRecieved = {
+                    status: "success!",
+                    your_data: {
+                        displayName: req.body.displayName.trim(),
+                    },
+                };
+                await User.findOneAndUpdate(
+                    { firstName: "Hello" },
+                    {
+                        displayName: req.body.displayName.trim(),
+                    }
+                );
                 res.json(dataRecieved);
             }
         } catch (err) {
