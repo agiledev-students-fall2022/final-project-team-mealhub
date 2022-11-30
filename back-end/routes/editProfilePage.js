2e47a155da616fe3a2b84f3e0fa808d3a1381c04
mishaseo <ms11568@nyu.edu>
2022-11-30 18:34:33 -0500

    Updated with authentication usage

--- back-end/routes/editProfilePage.js
+++ back-end/routes/editProfilePage.js
@@ -1,15 +1,14 @@
 const express = require("express");
-
+const cookieParser = require("cookie-parser");
 const router = express.Router();
-
+router.use(cookieParser());
 const User = require("../models/User");
 
-//const { checkUser } = require("../middleware_auth/jwt_auth");
+const { checkUser } = require("../middleware_auth/jwt_auth");
 
 const { body, validationResult } = require("express-validator");
 
-//CODE FOR WHEN AUTHENTICATION IS SET
-/*router.post(
+router.post(
     "/editInfo",
     checkUser,
     body("displayName")
@@ -30,45 +29,9 @@ const { body, validationResult } = require("express-validator");
                         displayName: req.body.displayName.trim(),
                     },
                 };
-                await User.findByIdAndUpdate(req.user.id, {
+                await User.findByIdAndUpdate(res.locals.user.id, {
                     displayName: req.body.displayName.trim(),
                 });
-                res.json(dataRecieved);
-            }
-        } catch (err) {
-            console.log(err);
-            res.json({
-                success: false,
-            });
-        }
-    }
-);*/
-
-router.post(
-    "/editInfo",
-    body("displayName")
-        .trim()
-        .isLength({ min: 1 })
-        .withMessage("Must be at least 1 character long"),
-    async (req, res) => {
-        try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(400).json({ errors: errors.array() });
-            } else {
-                // requesting resource from DB
-                const dataRecieved = {
-                    status: "success!",
-                    your_data: {
-                        displayName: req.body.displayName.trim(),
-                    },
-                };
-                await User.findOneAndUpdate(
-                    { firstName: "Hello" },
-                    {
-                        displayName: req.body.displayName.trim(),
-                    }
-                );
                 res.json(dataRecieved);
             }
         } catch (err) {
--- back-end/routes/profilePage.js
+++ back-end/routes/profilePage.js
@@ -1,37 +1,18 @@
 const express = require("express");
 
+const cookieParser = require("cookie-parser");
 const router = express.Router();
-
+router.use(cookieParser());
 const User = require("../models/User");
 
-//CODE FOR WHEN AUTHENTICATION IS SET
-/*const { checkUser } = require("../middleware_auth/jwt_auth");
-
+const { checkUser } = require("../middleware_auth/jwt_auth");
 
 //----------------------Fetch profile page api data-----------------------------------
 
 router.get("/profilePage", checkUser, async (req, res) => {
     try {
         // requesting resource from DB
-        const userInfo = await User.findById(req.user.id);
-        res.json({
-            displayName: userInfo.displayName,
-            firstName: userInfo.firstName,
-            lastName: userInfo.lastName,
-            email: userInfo.email,
-        }); // pass data along directly to client
-    } catch (err) {
-        res.json({
-            success: false,
-        });
-    }
-});
-*/
-
-router.get("/profilePage", async (req, res) => {
-    try {
-        // requesting resource from DB
-        const userInfo = await User.findOne({ firstName: "Hello" });
+        const userInfo = await User.findById(res.locals.user.id);
         res.json({
             image: userInfo.image,
             displayName: userInfo.displayName,
