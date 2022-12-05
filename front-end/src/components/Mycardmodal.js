2f6877eefd9ab419df19f18ab6f3cbb5bba2bee8
Dev Kalavadiya <67102702+Dev-Kalavadia@users.noreply.github.com>
2022-12-05 17:43:54 -0500

    Added delete button
    delete button added to the myGroups page modal

--- front-end/src/components/Mycardmodal.js
+++ front-end/src/components/Mycardmodal.js
@@ -1,76 +1,93 @@
 import React from "react";
 import { Button, Header, Image, Modal } from "semantic-ui-react";
-import "./cardmodal.css";
+import "./Mycardmodal.css";
+import axios from "axios";
 import dateFormat, { masks } from "dateformat";
 
 function CardModal(props) {
-	// const [open, setOpen] = React.useState(true);
-	const user = {
-		createdAt: "2022-11-19T06:22:11.461Z",
-		displayName: "Test Er",
-		email: "tester@gmail.com",
-		firstName: "Test",
-		lastName: "Er",
-		password: "$2b$10$fMEQ7TK.wsR0T3jBjjaQZOms9kyPU45wU1h2aocyQf9Q/NsVI/DEC",
-		__v: 0,
-		_id: "637876133b70b16a643dfb29",
-	};
-	return (
-		<div className="modal">
-			<Modal
-				size="small"
-				open={props.modalOpen}
-				closeOnEscape={true}
-				onClose={props.handleClose}
-				closeOnRootNodeClick={true}
-				closeOnDocumentClick={true}
-				// closeIcon={true}
-			>
-				<Modal.Content image>
-					<Image size="small" src={props.data.image} />
-					<Modal.Description>
-						{/* <Modal.Meta>
+  // const [open, setOpen] = React.useState(true);
+  const user = {
+    createdAt: "2022-11-19T06:22:11.461Z",
+    displayName: "Test Er",
+    email: "tester@gmail.com",
+    firstName: "Test",
+    lastName: "Er",
+    password: "$2b$10$fMEQ7TK.wsR0T3jBjjaQZOms9kyPU45wU1h2aocyQf9Q/NsVI/DEC",
+    __v: 0,
+    _id: "637876133b70b16a643dfb29",
+  };
+  return (
+    <div className="modal">
+      <Modal
+        size="small"
+        open={props.modalOpen}
+        closeOnEscape={true}
+        onClose={props.handleClose}
+        closeOnRootNodeClick={true}
+        closeOnDocumentClick={true}
+        // closeIcon={true}
+      >
+        <Modal.Content image>
+          <Image size="small" src={props.data.image} />
+          <Modal.Description>
+            {/* <Modal.Meta>
 							<span>{"12-12-12"}</span>
 						</Modal.Meta> */}
-						<span className="modal-span">
-							{dateFormat(props.data.date, "dddd, mmmm dS, yyyy, h:MM:ss TT")}
-						</span>
+            <span className="modal-span">
+              {dateFormat(props.data.date, "dddd, mmmm dS, yyyy, h:MM:ss TT")}
+            </span>
 
-						<Header>{props.data.restaurant}</Header>
-						<span className="modal-span">{props.data.location}</span>
-						<br></br>
-						<span className="modal-span">
-							{props.data.members == null ? (props.data.members = []) : null}
+            <Header>{props.data.restaurant}</Header>
+            <span className="modal-span">{props.data.location}</span>
+            <br></br>
+            <span className="modal-span">
+              {props.data.members == null ? (props.data.members = []) : null}
 
-							{+(props.data.capacity - props.data.members.length) * 1 +
-								" spots left"}
-						</span>
-					</Modal.Description>
-				</Modal.Content>
-				<Modal.Content>
-					<Modal.Description>
-						<Header>Description</Header>
-						<p>{props.data.description}</p>
-						<p>
-							{}
-							{"Cuisine Location: " +
-								props.data.cuisine.charAt(0).toUpperCase() +
-								props.data.cuisine.slice(1)}
-						</p>
-						<p>{"Budget: $" + props.data.budget}</p>
-						<p>{`Dress-code: ${props.data.dress_code}`}</p>
-						<p>
-							{props.data.user == null ? (props.data.user = user) : null}
-							{("" + props.data.members.length).replace(/^[0]+/g, "") +
-								"/" +
-								props.data.capacity}{" "}
-							MealHubers | {props.data.user.displayName}
-						</p>
-					</Modal.Description>
-				</Modal.Content>
-			</Modal>
-		</div>
-	);
+              {+(props.data.capacity - props.data.members.length) * 1 +
+                " spots left"}
+            </span>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Content>
+          <Modal.Description>
+            <Header>Description</Header>
+            <p>{props.data.description}</p>
+            <p>
+              {}
+              {"Cuisine Location: " +
+                props.data.cuisine.charAt(0).toUpperCase() +
+                props.data.cuisine.slice(1)}
+            </p>
+            <p>{"Budget: $" + props.data.budget}</p>
+            <p>{`Dress-code: ${props.data.dress_code}`}</p>
+            <p>
+              {props.data.user == null ? (props.data.user = user) : null}
+              {("" + props.data.members.length).replace(/^[0]+/g, "") +
+                "/" +
+                props.data.capacity}{" "}
+              MealHubers | {props.data.user.displayName}
+            </p>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            floated="right"
+            color="red"
+            onClick={() => {
+              axios
+                .delete(`${process.env.REACT_APP_URL}/myGroup/${props.data._id}`)
+                .then((response) => {
+                  console.log(props.data);
+                  window.location.href = "/myGroup";
+                });
+            }}
+          >
+            Delete
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
+  );
 }
 
 export default CardModal;
